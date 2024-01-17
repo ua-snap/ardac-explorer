fc9d664232b18e3756f9b9aa8a13dd3adf54aaf8
Craig Stephenson <crstephenson@alaska.edu>
2024-01-16 16:44:45 -0900

    Use Masonry library for dynamic card layout.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['assets/styles/main.scss']
+  css: ['assets/styles/main.scss'],
 })
