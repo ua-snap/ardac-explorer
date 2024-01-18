a81c56bf3e0e07a0cf84ca8c7fd933e5355e7a1b
Craig Stephenson <crstephenson@alaska.edu>
2024-01-18 06:39:18 -0900

    Read card content from a JSON file and group them into categories on front page (#7)
    * Resolve rebase conflict with main.
    
    * Add categories to JSON content and display them accordingly.
    
    * Switch from JSON(strict) to JS + minimize example somewhat
    
    ---------
    
    Co-authored-by: Bruce Crevensten <bruce.crevensten@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -5,222 +5,55 @@ definePageMeta({
 
 const { $Masonry } = useNuxtApp()
 
+import cards from '~/assets/cards'
+
+let cardsByCategory = {}
+cards.forEach(card => {
+  if (!cardsByCategory[card.category]) {
+    cardsByCategory[card.category] = []
+  }
+  cardsByCategory[card.category].push(card)
+})
+
+let categories = Object.keys(cardsByCategory).map(category => {
+  return {
+    name: category,
+    slug: category.toLowerCase().replaceAll(' ', '-'),
+  }
+})
+
 onMounted(() => {
-  new $Masonry('.grid', {
-    itemSelector: '.grid-item',
-    columnWidth: 300,
+  categories.forEach(category => {
+    let gridSelector = `.grid-${category['slug']}`
+    let gridItemSelector = `.grid-item-${category['slug']}`
+    new $Masonry(gridSelector, {
+      itemSelector: gridItemSelector,
+      columnWidth: 300,
+    })
   })
 })
 </script>
 
 <template>
-  <div class="grid">
-    <div class="card clamp grid-item mb-5">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img src="~/assets/images/data-preview-1.png" alt="A Map" />
-        </figure>
-      </div>
-      <div class="card-content">
-        <div class="content">
-          <h3 class="title is-4">Future precipitation map</h3>
-          <p>
-            <a href=""
-              >Access a <a href="#">web map service</a> combining projected
-              CMIP5 precipitation and USGS web map services. This can be used in
-              GIS programs to produce data summaries at a local scale.</a
-            >
-          </p>
-          <div>
-            <span class="tag is-dark">Precipitation</span>
-            <span class="tag is-dark">WMS</span>
-            <span class="tag is-dark">Visualization</span>
-            <span class="tag is-dark">GIS</span>
-          </div>
+  <div v-for="category in categories">
+    <h1 class="title is-3">{{ category['name'] }}</h1>
+    <div :class="'mb-3 grid-' + category['slug']">
+      <div
+        :class="'card clamp mb-5 grid-item-' + category['slug']"
+        v-for="card in cardsByCategory[category['name']]"
+      >
+        <div v-if="card['image']" class="card-image">
+          <figure class="image is-4by3">
+            <img :src="card['image']" :alt="card['image_alt']" />
+          </figure>
         </div>
-      </div>
-    </div>
-    <div class="card clamp grid-item mb-5">
-      <div class="card-content">
-        <div class="content">
-          <h3 class="title is-3">ALFRESCO flammability and vegetation</h3>
-          <p>
-            Access flammability and predominant vegetation type for a
-            latitude/longitude point or area of interest in JSON or CSV format
-            with <a href="https://earthmaps.io/alfresco/">this API endpoint</a>.
-          </p>
-          <div>
-            <span class="tag is-dark">Wildfire</span>
-            <span class="tag is-dark">Vegetation</span>
-            <span class="tag is-dark">API</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card clamp grid-item mb-5">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img src="~/assets/images/data-preview-1.png" alt="A Map" />
-        </figure>
-      </div>
-      <div class="card-content">
-        <div class="content">
-          <h3 class="title is-4">Future precipitation map</h3>
-          <p>
-            <a href=""
-              >Access a <a href="#">web map service</a> combining projected
-              CMIP5 precipitation and USGS web map services. This can be used in
-              GIS programs to produce data summaries at a local scale.</a
-            >
-          </p>
-          <div>
-            <span class="tag is-dark">Precipitation</span>
-            <span class="tag is-dark">WMS</span>
-            <span class="tag is-dark">Visualization</span>
-            <span class="tag is-dark">GIS</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card clamp grid-item mb-5">
-      <div class="card-content">
-        <div class="content">
-          <h3 class="title is-3">ALFRESCO flammability and vegetation</h3>
-          <p>
-            Access flammability and predominant vegetation type for a
-            latitude/longitude point or area of interest in JSON or CSV format
-            with <a href="https://earthmaps.io/alfresco/">this API endpoint</a>.
-          </p>
-          <div>
-            <span class="tag is-dark">Wildfire</span>
-            <span class="tag is-dark">Vegetation</span>
-            <span class="tag is-dark">API</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card clamp grid-item mb-5">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img src="~/assets/images/data-preview-1.png" alt="A Map" />
-        </figure>
-      </div>
-      <div class="card-content">
-        <div class="content">
-          <h3 class="title is-4">Future precipitation map</h3>
-          <p>
-            <a href=""
-              >Access a <a href="#">web map service</a> combining projected
-              CMIP5 precipitation and USGS web map services. This can be used in
-              GIS programs to produce data summaries at a local scale.</a
-            >
-          </p>
-          <div>
-            <span class="tag is-dark">Precipitation</span>
-            <span class="tag is-dark">WMS</span>
-            <span class="tag is-dark">Visualization</span>
-            <span class="tag is-dark">GIS</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card clamp grid-item mb-5">
-      <div class="card-content">
-        <div class="content">
-          <h3 class="title is-3">ALFRESCO flammability and vegetation</h3>
-          <p>
-            Access flammability and predominant vegetation type for a
-            latitude/longitude point or area of interest in JSON or CSV format
-            with <a href="https://earthmaps.io/alfresco/">this API endpoint</a>.
-          </p>
-          <div>
-            <span class="tag is-dark">Wildfire</span>
-            <span class="tag is-dark">Vegetation</span>
-            <span class="tag is-dark">API</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card clamp grid-item mb-5">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img src="~/assets/images/data-preview-1.png" alt="A Map" />
-        </figure>
-      </div>
-      <div class="card-content">
-        <div class="content">
-          <h3 class="title is-4">Future precipitation map</h3>
-          <p>
-            <a href=""
-              >Access a <a href="#">web map service</a> combining projected
-              CMIP5 precipitation and USGS web map services. This can be used in
-              GIS programs to produce data summaries at a local scale.</a
-            >
-          </p>
-          <div>
-            <span class="tag is-dark">Precipitation</span>
-            <span class="tag is-dark">WMS</span>
-            <span class="tag is-dark">Visualization</span>
-            <span class="tag is-dark">GIS</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card clamp grid-item mb-5">
-      <div class="card-content">
-        <div class="content">
-          <h3 class="title is-3">ALFRESCO flammability and vegetation</h3>
-          <p>
-            Access flammability and predominant vegetation type for a
-            latitude/longitude point or area of interest in JSON or CSV format
-            with <a href="https://earthmaps.io/alfresco/">this API endpoint</a>.
-          </p>
-          <div>
-            <span class="tag is-dark">Wildfire</span>
-            <span class="tag is-dark">Vegetation</span>
-            <span class="tag is-dark">API</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card clamp grid-item mb-5">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img src="~/assets/images/data-preview-1.png" alt="A Map" />
-        </figure>
-      </div>
-      <div class="card-content">
-        <div class="content">
-          <h3 class="title is-4">Future precipitation map</h3>
-          <p>
-            <a href=""
-              >Access a <a href="#">web map service</a> combining projected
-              CMIP5 precipitation and USGS web map services. This can be used in
-              GIS programs to produce data summaries at a local scale.</a
-            >
-          </p>
-          <div>
-            <span class="tag is-dark">Precipitation</span>
-            <span class="tag is-dark">WMS</span>
-            <span class="tag is-dark">Visualization</span>
-            <span class="tag is-dark">GIS</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card clamp grid-item mb-5">
-      <div class="card-content">
-        <div class="content">
-          <h3 class="title is-3">ALFRESCO flammability and vegetation</h3>
-          <p>
-            Access flammability and predominant vegetation type for a
-            latitude/longitude point or area of interest in JSON or CSV format
-            with <a href="https://earthmaps.io/alfresco/">this API endpoint</a>.
-          </p>
-          <div>
-            <span class="tag is-dark">Wildfire</span>
-            <span class="tag is-dark">Vegetation</span>
-            <span class="tag is-dark">API</span>
+        <div class="card-content">
+          <div class="content">
+            <h3 class="title is-4">{{ card['title'] }}</h3>
+            <p v-html="card['blurb']" />
+            <span v-for="tag in card['tags']" class="tag is-dark mb-1">{{
+              tag
+            }}</span>
           </div>
         </div>
       </div>
