2f72872a9801681e22993959f28d3939f9b4327f
Craig Stephenson <crstephenson@alaska.edu>
2024-01-17 16:20:01 -0900

    Add categories to JSON content and display them accordingly.

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,60 @@
 <script lang="ts" setup>
-import cards from '~/assets/cards.json'
 definePageMeta({
   layout: 'home',
 })
 
 const { $Masonry } = useNuxtApp()
 
+import cards from '~/assets/cards.json'
+
+let cardsByCategory = {}
+cards.forEach(card => {
+  if (!cardsByCategory[card.category]) {
+    cardsByCategory[card.category] = []
+  }
+  cardsByCategory[card.category].push(card)
+})
+
+let categories = Object.keys(cardsByCategory).map(category => {
+  return {
+    name: category,
+    slug: category.toLowerCase().replaceAll(' ', '-'),
+  }
+})
+
 onMounted(() => {
-  new $Masonry('.grid', {
-    itemSelector: '.grid-item',
-    columnWidth: 300,
+  categories.forEach(category => {
+    let gridSelector = `.grid-${category['slug']}`
+    let gridItemSelector = `.grid-item-${category['slug']}`
+    new $Masonry(gridSelector, {
+      itemSelector: gridItemSelector,
+      columnWidth: 300,
+    })
   })
 })
 </script>
 
 <template>
-  <div class="grid">
-    <div class="card clamp grid-item mb-5" v-for="card in cards">
-      <div v-if="card['image']" class="card-image">
-        <figure class="image is-4by3">
-          <img :src="card['image']" :alt="card['image_alt']" />
-        </figure>
-      </div>
-      <div class="card-content">
-        <div class="content">
-          <h3 class="title is-4">{{ card['title'] }}</h3>
-          <p v-html="card['blurb']" />
-          <span v-for="tag in card['tags']" class="tag is-dark mb-1">{{
-            tag
-          }}</span>
+  <div v-for="category in categories">
+    <h1 class="title is-3">{{ category['name'] }}</h1>
+    <div :class="'mb-3 grid-' + category['slug']">
+      <div
+        :class="'card clamp mb-5 grid-item-' + category['slug']"
+        v-for="card in cardsByCategory[category['name']]"
+      >
+        <div v-if="card['image']" class="card-image">
+          <figure class="image is-4by3">
+            <img :src="card['image']" :alt="card['image_alt']" />
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            <h3 class="title is-4">{{ card['title'] }}</h3>
+            <p v-html="card['blurb']" />
+            <span v-for="tag in card['tags']" class="tag is-dark mb-1">{{
+              tag
+            }}</span>
+          </div>
         </div>
       </div>
     </div>
