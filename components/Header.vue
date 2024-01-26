397a5a3248dbbadf974e9276c853def8f7bdf662
Bruce Crevensten <bruce.crevensten@gmail.com>
2024-01-26 13:57:05 -0900

    Improve structure of layout/page components

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,40 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    <span>Ar</span>ctic <span>Da</span>ta <span>C</span>ollaborative
-  </div>
+  <section class="section dark">
+    <div class="brand mb-6">
+      <h1>Arctic Data Collaborative</h1>
+      <h2>
+        <img
+          src="~/assets/images/ardac-green-logo.svg"
+          alt="logo of ARDAC acronym"
+        /><span>Explorer</span>
+      </h2>
+    </div>
+  </section>
 </template>
 
 <style lang="scss" scoped>
-  span {
-    font-weight: 900;
+.brand {
+  color: $white;
+  h1 {
+    color: #ccc;
+    margin-left: 4rem;
+    margin-bottom: -2.2rem;
+    font-weight: 700;
+    font-size: 2.3rem;
   }
+  h2 {
+    font-weight: 900;
+    font-size: 6rem;
+    span {
+      position: relative;
+      bottom: 5px;
+    }
+    img {
+      height: 4.62rem;
+    }
+  }
+}
+
 </style>
