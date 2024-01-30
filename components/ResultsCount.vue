e7d8fd421ee432f406b57106a0bc4884dbcec4b3
Bruce Crevensten <bruce.crevensten@gmail.com>
2024-01-29 16:02:39 -0900

    Remove styling to white text

--- components/ResultsCount.vue
+++ components/ResultsCount.vue
@@ -11,18 +11,18 @@ const totalItemCount = computed(() => store.totalItemCount)
     <div v-if="resultsCount > 0">
       <p class="ml-2 mt-2">
         Showing
-        <strong class="has-text-white">{{ resultsCount }}</strong>
+        <strong>{{ resultsCount }}</strong>
         <span v-if="resultsCount > 1"> matches</span>
         <span v-else> match</span>
         out of
-        <strong class="has-text-white">{{ totalItemCount }}</strong>
+        <strong>{{ totalItemCount }}</strong>
         possible items.
       </p>
     </div>
     <div v-else>
       <p class="ml-2 mt-2">
         No matches found out of
-        <strong class="has-text-white">{{ totalItemCount }}</strong>
+        <strong>{{ totalItemCount }}</strong>
         possible items.
       </p>
     </div>
