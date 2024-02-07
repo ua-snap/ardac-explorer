c58c3d63ee1ac59e7c2609cd7c221c921975005f
Craig Stephenson <crstephenson@alaska.edu>
2024-02-07 12:12:38 -0900

    Move noscript HTML into nuxt.config.ts.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
 import { resolve } from 'path'
 
+const noscriptHtml = `
+<section class="section">
+<div class="container">
+  <h1 class="title is-1">Arctic Data Collaborative</h1>
+  <div class="content is-size-5">
+    <p>The Arctic Data Collaborative (ARDAC) provides highly-curated,
+        relevant <b>Arctic datasets</b>, streamlined <b>data access</b>,
+        processing <code>code examples</code> and <b>visualizations</b> to
+        support research in the North.
+    </p>
+    <p>
+      ⚠️ We&rsquo;re sorry, but this web tool requires
+      Javascript to be enabled to run. <strong>
+        Please email us at
+        uaf-snap-data-tools@alaska.edu
+      </strong> if you would like assistance to
+      access content on this site.
+    </p>
+  </div>
+</div>
+</section>
+`
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false, // required or 3rd party libs break the app
@@ -33,6 +56,15 @@ export default defineNuxtConfig({
       apiUrl: process.env.SNAP_API_URL || 'https://earthmaps.io',
       rasdamanUrl:
         process.env.RASDAMAN_URL || 'https://maps.earthmaps.io/rasdaman/ows',
+    },
+  },
+  app: {
+    head: {
+      noscript: [
+        {
+          children: noscriptHtml,
+        },
+      ],
     },
   },
 })
