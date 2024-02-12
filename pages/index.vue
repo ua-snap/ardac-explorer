591f085c377339eeda00a24d7056bad1d0192c1e
Craig Stephenson <crstephenson@alaska.edu>
2024-02-12 13:03:28 -0900

    Tweak dashed borders and padding.

--- pages/index.vue
+++ pages/index.vue
@@ -8,80 +8,81 @@ definePageMeta({
 
 <template>
   <section class="section">
-    <div class="container">
-      <div class="columns">
-        <div class="briefs column is-3">
-          <div class="content is-size-5 hello">
-            <p>
-              Hello! Not sure what you&rsquo;re looking for?
-              <a href="example.com">Email us!</a>
-            </p>
-          </div>
-          <ItemBrief :item="items[0]" />
-          <ItemBrief :item="items[1]" />
-          <ItemBrief :item="items[2]" />
-          <ItemBrief :item="items[3]" />
+    <div class="columns">
+      <div class="briefs column is-3 pr-5 border-right">
+        <div class="content is-size-5 hello">
+          <p>
+            Hello! Not sure what you&rsquo;re looking for?
+            <a href="example.com">Email us!</a>
+          </p>
         </div>
-        <div class="featured column is-6">
-          <div class="tile is-ancestor">
-            <div class="tile is-parent is-vertical">
-              <div class="tile is-child">
-                <ItemTextPicture :item="items[5]" />
-              </div>
-              <div class="general">
-                <div class="row tile is-parent mb-4">
-                  <div class="left tile is-child">
-                    <ItemText :item="items[4]" />
-                  </div>
-                  <div class="right tile is-child">
-                    <ItemText :item="items[6]" />
-                  </div>
+        <ItemBrief :item="items[0]" />
+        <ItemBrief :item="items[1]" />
+        <ItemBrief :item="items[2]" />
+        <ItemBrief :item="items[3]" />
+      </div>
+      <div class="featured column is-6 px-5">
+        <div class="tile is-ancestor">
+          <div class="tile is-parent is-vertical">
+            <div class="tile is-child border-bottom">
+              <ItemTextPicture :item="items[5]" />
+            </div>
+            <div class="general">
+              <div class="row tile is-parent mb-4">
+                <div class="left tile is-child">
+                  <ItemText :item="items[4]" />
                 </div>
-                <div class="tile is-parent">
-                  <div class="left tile is-child">
-                    <ItemText :item="items[7]" />
-                  </div>
-                  <div class="right tile is-child">
-                    <ItemText :item="items[8]" />
-                  </div>
+                <div class="right tile is-child">
+                  <ItemText :item="items[6]" />
+                </div>
+              </div>
+              <div class="tile is-parent">
+                <div class="left tile is-child">
+                  <ItemText :item="items[7]" />
+                </div>
+                <div class="right tile is-child">
+                  <ItemText :item="items[8]" />
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="column">
-          <div class="content is-size-5">
-            <p>Did you know people are awesome?</p>
-            <ul>
-              <li>Sign up for our newsletter</li>
-              <li>Email us with questions</li>
-              <li>Visit data API</li>
-              <li>
-                <NuxtLink
-                  href="https://archive.org/details/INTERNETCLUB-REDEFININGTHEWORKPLACE/04+SYNERGIZE.mp3"
-                  >Redefining the workplace</NuxtLink
-                >
-              </li>
-              <li>Who are these people anyways?</li>
-              <li>&hellip;and other content.</li>
-            </ul>
-          </div>
+      </div>
+      <div class="column right pl-5 border-left">
+        <div class="content is-size-5">
+          <p>Did you know people are awesome?</p>
+          <ul>
+            <li>Sign up for our newsletter</li>
+            <li>Email us with questions</li>
+            <li>Visit data API</li>
+            <li>
+              <NuxtLink
+                href="https://archive.org/details/INTERNETCLUB-REDEFININGTHEWORKPLACE/04+SYNERGIZE.mp3"
+                >Redefining the workplace</NuxtLink
+              >
+            </li>
+            <li>Who are these people anyways?</li>
+            <li>&hellip;and other content.</li>
+          </ul>
         </div>
       </div>
     </div>
   </section>
   <hr />
-  <section class="section topic">
-    <h2 class="title is-2">Terrestrial</h2>
+  <h2 class="title is-2 mx-6">Terrestrial</h2>
+  <hr class="light" />
+  <section class="section topic pt-3">
     <div class="tile is-ancestor">
-      <div class="tile is-vertical is-parent">
-        <div class="tile is-child"><ItemText :item="items[9]" /></div>
+      <div class="tile is-vertical is-parent pr-5 border-right">
+        <div class="tile is-child border-bottom">
+          <ItemText :item="items[9]" />
+        </div>
         <div class="tile is-child"><ItemText :item="items[10]" /></div>
       </div>
-      <div class="tile is-parent">
+      <div class="tile is-parent px-5">
         <div class="tile is-child"><ItemTextPicture :item="items[11]" /></div>
       </div>
-      <div class="tile is-parent">
+      <div class="tile is-parent pl-5 border-left">
         <div class="tile is-child"><ItemTextPicture :item="items[1]" /></div>
       </div>
     </div>
@@ -92,10 +93,10 @@ definePageMeta({
 .hello {
   background-color: $white-darker;
   padding: 1rem;
-  border: 2px dashed $gray-darker;
+  border: 1px dashed $gray-lighter;
 }
 .tile.row {
-  border-bottom: 1px dotted $gray-darker;
+  border-bottom: 1px dashed $gray-lighter;
 }
 :deep(.item) {
   p {
@@ -124,7 +125,7 @@ definePageMeta({
 .general .tile.left {
   &:deep(.item) {
     padding-right: 1rem;
-    border-right: 1px dotted $gray-darker;
+    border-right: 1px dashed $gray-lighter;
   }
 }
 .general .tile.right {
@@ -137,5 +138,17 @@ definePageMeta({
       min-height: 5rem;
     }
   }
+}
+
+.border-left {
+  border-left: 1px dashed $gray-lighter;
+}
+
+.border-right {
+  border-right: 1px dashed $gray-lighter;
+}
+
+.border-bottom {
+  border-bottom: 1px dashed $gray-lighter;
 }
 </style>
