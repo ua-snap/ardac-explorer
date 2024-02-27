b3d916822ef35097ce9c68c9e547172e7dc3da5c
Craig Stephenson <crstephenson@alaska.edu>
2024-02-27 10:47:07 -0900

    Add dashed column borders to Cryosphere section on front page.

--- components/FrontPage/Section/Cryosphere.vue
+++ components/FrontPage/Section/Cryosphere.vue
@@ -3,13 +3,13 @@
 <template>
   <FrontPageSectionWrapper tag="Cryosphere">
     <div class="columns">
-      <div class="column">
+      <div class="column border-right">
         <ItemTextPicture slug="landfast-sea-ice" />
       </div>
       <div class="column">
         <ItemTextPicture slug="sea-ice-concentration" />
       </div>
-      <div class="column">
+      <div class="column border-left">
         <ItemTextPicture slug="notebook-sea-ice-traffic" />
       </div>
     </div>
