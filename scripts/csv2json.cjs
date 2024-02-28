1f2f4ba2233107cbdd807b26ee1d4ba4729c9c9e
Bruce Crevensten <bruce.crevensten@gmail.com>
2024-02-28 13:02:11 -0800

    Adds more items in bulk from spreadsheet

--- scripts/csv2json.cjs
+++ scripts/csv2json.cjs
@@ -10,42 +10,50 @@ let tagSet = new Set()
 
 console.log('type Slug =')
 csv({
-  ignoreColumns: /Who|What|Notes/, // ignore unused columns
+  ignoreColumns: /drop/, // ignore unused columns
 })
   .fromFile(csvFile)
   .subscribe(json => {
     return new Promise((resolve, reject) => {
       // Generate most of the needed JSON in bulk
+      
+      /* Generate the Items 
       let tags = json.tags.split(', ')
       
       tags.forEach((tag, i) => {
         tags[i] = tag.trim()
       })
-      
-      console.log(
+
+      let item = 
         {
           slug: json.slug,
           title: json.title,
           blurb: json.blurb,
           tags: tags,
-        },
-        ','
-      )
-      
+         
+        }
+
+        if (json.image && json.image_alt ) {
+          item.image = json.image;
+          item.imageAlt = json.image_alt
+        }
+        console.log(item)
+        console.log(',')
+      */
 
       /* Regenerate valid slugs */
-      // console.log("  | '" + json.slug + "'")
+      console.log("  | '" + json.slug + "'")
 
       /* Add tags to set to be output later */
-      tags.forEach(tag => {
-        tagSet.add(tag)
-      })
+      // tags.forEach(tag => {
+      //   tagSet.add(tag)
+      // })
       resolve()
     })
   })
   .then(() => {
-    console.log('\ntype Tag =')
-    tagSet.forEach(tag => {
-      console.log("  | '" + tag + "'")
-    })
+    // console.log('\ntype Tag =')
+    // tagSet.forEach(tag => {
+    //   console.log("  | '" + tag + "'")
+    // })
   })
