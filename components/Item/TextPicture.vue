7ab04ba933e26c06f398f7271cea920a019a7355
Bruce Crevensten <bruce.crevensten@gmail.com>
2024-03-07 09:22:19 -0800

    Prevent navigation to items without content implemented

--- components/Item/TextPicture.vue
+++ components/Item/TextPicture.vue
@@ -9,15 +9,27 @@ let item = store.itemBySlug(props.slug)
 </script>
 
 <template>
-  <div class="item text-picture">
+  <div v-if="store.itemHasComponent(item)" class="item text-picture">
     <Tag v-if="showTag" :tag="item.tags[0]" />
     <NuxtLink :to="{ name: 'item-slug', params: { slug: item.slug } }">
       <h3 class="title is-4" v-html="item.title"></h3>
       <p v-html="item.blurb" class="mb-4" />
       <figure v-if="item.image" class="image is-5by4">
-        <img :src="'/previews/' + item.image" :alt="item.imageAlt" class="mb-4" />
+        <img
+          :src="'/previews/' + item.image"
+          :alt="item.imageAlt"
+          class="mb-4"
+        />
       </figure>
     </NuxtLink>
+  </div>
+  <div v-else class="item text-picture missing" title="Content not implemented yet">
+    <Tag v-if="showTag" :tag="item.tags[0]" />
+    <h3 class="title is-4" v-html="item.title"></h3>
+    <p v-html="item.blurb" class="mb-4" />
+    <figure v-if="item.image" class="image is-5by4">
+      <img :src="'/previews/' + item.image" :alt="item.imageAlt" class="mb-4" />
+    </figure>
   </div>
 </template>
 
