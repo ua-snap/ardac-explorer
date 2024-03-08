4949f36aaba08f97c6242bd13b335f1c521256dd
Bruce Crevensten <bruce.crevensten@gmail.com>
2024-03-08 12:08:16 -0800

    Prevent navigation to items without content implemented (#44)

--- components/Item/Brief.vue
+++ components/Item/Brief.vue
@@ -6,13 +6,19 @@ const props = defineProps<{
 
 const store = useStore()
 let item = store.itemBySlug(props.slug)
+
 </script>
 
 <template>
-  <div class="item brief">
+  <div v-if="store.itemHasComponent(item)" class="item brief">
     <Tag v-if="showTag" :tag="item.tags[0]" />
     <NuxtLink :to="{ name: 'item-slug', params: { slug: item.slug } }">
       <h3 class="title is-5" v-html="item.title"></h3>
     </NuxtLink>
+  </div>
+  <div v-else class="item brief missing" title="Content not impemented yet">
+    <!-- If missing component -->
+    <Tag v-if="showTag" :tag="item.tags[0]" />
+    <h3 class="title is-5" v-html="item.title"></h3>
   </div>
 </template>
