f2feec131a1b4d4c425d3c748fbfcfda8ea9e06d
Craig Stephenson <crstephenson@alaska.edu>
2024-03-19 16:58:12 -0800

    Intercept and redirect S3 hashbang URLs.

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,14 @@
 definePageMeta({
   layout: 'home',
 })
+
+// Redirect S3 hashbang URLs to Nuxt URLs.
+const route = useRoute()
+onMounted(() => {
+  if (route.hash.substring(0, 3) == '#!/') {
+    navigateTo(route.hash.substring(2), { external: true })
+  }
+})
 </script>
 
 <template>
