9df5a5e3c638008a5b3df28c7cc9b634349a93dc
Craig Stephenson <crstephenson@alaska.edu>
2024-03-19 18:06:14 -0800

    Move S3 redirect into new app.vue to fix unrecognized 404 issue.

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,6 @@
 definePageMeta({
   layout: 'home',
 })
-
-// Redirect S3 hashbang URLs to Nuxt URLs.
-const route = useRoute()
-onMounted(() => {
-  if (route.hash.substring(0, 3) == '#!/') {
-    navigateTo(route.hash.substring(2), { external: true })
-  }
-})
 </script>
 
 <template>
