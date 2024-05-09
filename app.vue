458e1c8b229961bea79e941978de0d0da7d3b2e7
Craig Stephenson <crstephenson@alaska.edu>
2024-05-09 14:11:53 -0800

    Redirect S3 hashbang path redirects to their corresponding Nuxt paths (#52)
    * Intercept and redirect S3 hashbang URLs.
    
    * Move S3 redirect into new app.vue to fix unrecognized 404 issue.
    
    * Use onBeforeMount instead of onMounted to avoid flash-of-front-page.

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+// Redirect S3 hashbang URLs to Nuxt URLs.
+const route = useRoute()
+onBeforeMount(() => {
+  if (route.hash.substring(0, 3) == '#!/') {
+    navigateTo(route.hash.substring(2))
+  }
+})
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
