d1ed18a3f3e2b226dfc0c3f147749c2fe5e0dc84
MikeDelue <mdelue@alaska.edu>
2024-05-14 11:42:46 -0800

    Changes heading formatting and add's Carolyn's image on voltinism

--- components/global/BeetleRiskStory.vue
+++ components/global/BeetleRiskStory.vue
@@ -4,7 +4,7 @@
       <h3 class="title is-3">Climate Protection from Spruce Beetles, by Bob</h3>
       <p class="mb-6">
       </p>
-      <p>Introduction</p>
+      <h4>Introduction</h4>
       <p>
         Spruce beetles are small insects that play a significant role in Alaska's 
         forests, infesting white spruce, Sitka spruce, and Lutz spruce. 
@@ -14,7 +14,7 @@
         researchers to delve deeper into understanding the factors influencing these
          outbreaks.
       </p>
-      <p>The Beetle Life Cycle and Climate Connection</p>
+      <h4>The Beetle Life Cycle and Climate Connection</h4>
       <p>
         Spruce beetles have a lifecycle closely tied to climate conditions. Adult beetles 
         typically fly to new trees in the spring, where females lay eggs. The timing of beetle 
@@ -27,15 +27,18 @@
         Beetles are also moderated by cold temperatures.  Rapid cooling during the fall or extreme cold 
         temperatures during winter can limit beetle survival. Understanding these climate factors is essential 
         for predicting and mitigating outbreaks.
-
       </p>
 
-      <p>Modeling Spruce Beetle Outbreaks</p>
+      <div>
+        <img src="assets/images/beetle_voltinism.png">
+      </div>
+
+      <h4>Modeling Spruce Beetle Outbreaks</h4>
       
       <p>To better understand and predict spruce beetle outbreaks, researchers developed a comprehensive model that incorporates various climate factors and beetle life cycle stages. The model assesses the risk of outbreaks across different time periods, considering historical climate data, climate projections, and beetle population dynamics. By calibrating the model with observed outbreak patterns, researchers categorized climate protection levels and identified regions that might lose past climate-linked protections from outbreaks.</p>
-      <p>Implications for Forest Management</p>
+      <h4>Implications for Forest Management</h4>
       <p>Understanding the factors driving spruce beetle outbreaks is essential for effective forest management and conservation efforts. By identifying areas that may become more vulnerable, forest managers can implement proactive measures to mitigate beetle infestations, such as targeted tree removal, forest thinning, and monitoring programs. These management practices can help maintain the health and resilience of Alaska's forests in the face of changing climate conditions.</p>
-      <p>Conclusion</p>
+      <h4>Conclusion</h4>
       <p>Spruce beetle outbreaks pose significant challenges to Alaska's forests, but through interdisciplinary research and advanced modeling techniques, scientists are gaining valuable insights into the complex interactions between climate, beetle populations, and forest ecosystems. By applying this knowledge, stakeholders can work towards sustainable forest management practices that help protect Alaska's valuable spruce forests for future generations.</p>
 
       </div>
