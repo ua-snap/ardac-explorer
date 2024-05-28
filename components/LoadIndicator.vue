7bb7b1b1ffddc75b7666ad2233fee75b791efa4d
Craig Stephenson <crstephenson@alaska.edu>
2024-05-28 13:16:24 -0800

    Add a progress bar when chart data is loading.

---
+++ components/LoadIndicator.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+const placesStore = usePlacesStore()
+const dataStore = useDataStore()
+</script>
+
+<template>
+  <div v-if="placesStore.latLng && !dataStore.apiData && !dataStore.dataError">
+    <progress class="progress" />
+  </div>
+</template>
