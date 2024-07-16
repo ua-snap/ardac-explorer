deafa70c11f98c376135533708525fff96bcd7da
MikeDelue <mdelue@alaska.edu>
2024-07-16 11:38:11 -0800

    Text edits and changes to climate stripes. Last commit until I get new graphics from Rick Thoman this afternoon.

--- components/global/StoryClimateStripes1.vue
+++ components/global/StoryClimateStripes1.vue
@@ -154,14 +154,14 @@ onUnmounted(() => {
       <h4>What are they and where do they come from?</h4>
       <p>
         <img src="assets/images/ClimateStripesStory1/ClimateStripesCollage.png">
-      <p>The graphic is a great example of convergent ideas with two scientists at the University of Reading and another at the University of Georgia seemingly independently coming up with the idea. Here at the University of Alaska Fairbanks climate scientist Rick Thoman has been using them to tell stories about climate for years.</p>
+      <p>The graphic is a great example of convergent ideas with two scientists at the University of Reading and another at the University of Georgia seemingly independently coming up with the idea. Here at the University of Alaska Fairbanks <a href="https://uaf-iarc.org/2022/07/iarcs-giant-climate-stripes-boldly-proclaim-arctic-change">IARC climate scientists like Rick Thoman and John Walsh</a> have been using them to tell stories about climate for years.</p>
       <p>In these data visualizations each stripe represents a year, with the color of the bar determined by the average temperature for that year. Typically this is the average temperature for the year at 2m elevation, or just over average head height. The stripes are colored with the darkest blue for the coldest year in that location, to the brightest red for the hottest year in that location.</p>
-      <p>Put it on a sticker or a wall and there you go. An arresting data visualization of temperature. The world’s getting warmer. But what else can we learn from these stripes by looking just a tiny bit deeper?</p>
+      <p>Put it on a sticker or a wall and there you go; an arresting data visualization of temperature. The world’s getting warmer. But what else can we learn from these stripes by looking just a tiny bit closer?</p>
 
       </p>
       <h4>Similar trends are apparent over different temperature ranges</h4>
       <p>
-        <p>Here’s a climate stripes graphic using gridded modeled historical data for Utqiagvik, the northernmost community in the United States on the north slope of Alaska:</p>
+        <p>Here’s a climate stripes graphic using gridded modeled historical data for Utqia&gdot;vik, the northernmost community in the United States on the north slope of Alaska:</p>
 
         <img src="assets/images/ClimateStripesStory1/Utqiagvik Climate Stripes 2022.png">
 
@@ -169,29 +169,34 @@ onUnmounted(() => {
 
 <img src="assets/images/beetle_voltinism.png">
 
-<p>Mean annual temperatures in Utqiagvik are around -10°C (15°F) while in Metlakatla, the southernmost community in the state near the border with British Columbia, the annual average temperature is closer to +10C (50°F). The common color palette across very different places, with different baseline climates, allows us to compare warming trends easily across latitudes and climates.</p>
+<p>Mean annual temperatures in Utqiagvik are around -10°C (15°F) while in Metlakatla, the southernmost community in the state near the border with British Columbia, the annual average temperature is closer to +10°C (50°F). The common color palette across very different places, with different baseline climates, allows us to compare warming trends easily across latitudes and climates.</p>
 
 <p>Looking across the world we can see that this means that in spite of their different climate both Maine and Arizona are warming, though this means different things for each place: </p>
-<p>less snow for snow-machining in Maine...</p><img src="assets/images/ClimateStripesStory1/ME-tavg.png">
-<p> ...and possible water scarcity in Arizona...</p><img src="assets/images/ClimateStripesStory1/AZ-tavg.png">
+<p>...a shorter snow-machining season in Maine...</p><img src="assets/images/ClimateStripesStory1/ME-tavg.png">
+<p> ...but 100°F+ summer days in Arizona straining the power grid...</p><img src="assets/images/ClimateStripesStory1/AZ-tavg.png">
 
       </p>
 
       <h4>Year-to-year variation is obvious, but trends are pronounced</h4>
 
-      <p>Okay okay, let’s stick closer to home. Alaska’s state capitol, Juneau, is a very neat but also pretty weird place. Off the road system, the state’s 3rd largest city is surrounded by mountains, islands, and America’s largest national forest. Here are the climate stripes for Juneau:</p>
+      <p>Okay, let’s stick closer to home. Alaska’s state capitol, Juneau, is a very neat but also pretty unique place. Off the road system, the state’s 3rd largest city is surrounded by mountains, islands, and America’s largest national forest. Here are the climate stripes for Juneau:</p>
       <img src="assets/images/ClimateStripesStory1/Juneau Climate Stripes 2022.png">
       <p>The variation of local weather is obvious across these stripes. This deep blue line represents 1972, the coldest year in the record with an average annual temperature of just 3.7°C (39°F). The warmest year though, is the most recent for this data…2017.</p>
-      <p>Overall though the trend is clear. Some warmer than average years do exist in the beginning of the record and a few colder than average years occurred more recently, but overall a trend toward more red and less blue is obvious across the stripes. Year to year variability can be caused by specific storm patterns, recurring cycles like el nino, or just plain chance. Understanding this variability is key to understanding why climate science comes with ranges and variability built in.</p>
+      <p>Overall though the trend is clear. Some warmer than average years do exist in the early 1900's and a few colder than average years occurred more recently, but overall a trend toward more red and less blue is obvious across the stripes. Year to year variability can be caused by specific storm patterns, recurring cycles like el ni&ntilde;o, or just plain chance. Understanding this variability is key to understanding why climate science comes with ranges and variability built in.</p>
       <p>The world is messy and our data should reflect that.</p>
+      <h4>Try for yourself!</h4>
+      <p>This module will let you create climate stripes for your part of Alaska. Maybe your home town, your hunting spot, your ethnolinguistic region, or even your lifelong favorite hydrological unit code level 10! Ours is 1908040302.</p>
+      <p></p>
+      <Gimme label="Get climate stripes for a community or by lat/long:" class="mt-5" />
+      <div id="chart" class="mb-5"></div>
 
       <h4>Variation is smoothed by scale</h4>
       <p>As humans we’re only ever in one place at any given time, so the climate stripes of your home community feel powerful. You remember that warm year with no snow, or that frigid year that drove up your heating bill. This is powerful, but data also allows us to expand beyond the single point and understand our local climate in the context of the trends at broader scales.</p>
       <p>288,000 Alaskans live in our largest city, Anchorage. Here’s what the climate stripes for the City and Borough of Anchorage look like:</p>
       <img src="assets/images/beetle_voltinism.png">
-      <p>But most folks who know the state will tell you that Anchorage is part of a broader ‘suburban core’ in the south-central part of Alaska. While it can be hard to draw precise lines around the “Greater Anchorage Area” we’ll expand out using a climatological perspective.</p>
+      <p>But most folks who know the state will tell you that Anchorage is part of a broader ‘population core’ in the south-central part of Alaska. While it can be hard to draw precise lines around this population core we’ll expand out using a climatological perspective.</p>
       <img src="assets/images/ClimateStripesStory1/CookInletMap.png">
-      <p>The “Cook Inlet Climate Division” encompasses most of the larger towns of the area including Soldotna, Wasilla, Palmer, and Homer. All told, this region covers the Matanuska-Susitna Valley and the northern half of the Kenai Peninsula hemmed in by mountains on three sides and with its local climate dominated by the moderating waters of the Cook Inlet.</p>
+      <p>The “Cook Inlet Climate Division” encompasses most of the larger towns of the area including Soldotna, Wasilla, Palmer, and Homer. All told this region covers the Matanuska-Susitna Valley and the northern half of the Kenai Peninsula hemmed in by mountains on three sides and with its local climate dominated by the temperature-moderating waters of the Cook Inlet.</p>
       <p>Here’s what the climate stripes for this area with a population of over 400,000, well over half the state, looks like:</p>
       <img src="assets/images/beetle_voltinism.png">
       <p>Local weather and unique climates can seriously affect how climate change in these stripes look at the community level. A loss of sea ice cover or a shortening winter snow cover season may amplify the change.</p>
@@ -206,7 +211,7 @@ onUnmounted(() => {
       <p>The trend dominates the graphic. We got these non-Arctic stripes from <a href="https://showyourstripes.info">another tool</a> by the way. We don't handle global data.</p>
 
       <h4>The power, and shortcomings, of a good climate graphic</h4>
-      <p>Overall the Climate Stripes graphics for points and for regions are a powerful tool. They are (mostly) colorblind-friendly and show clearly and obviously the facts of climate change: the trend toward a warmer climate is both clear and striking.</p>
+      <p>Overall the Climate Stripes graphics for points and for regions are a powerful tool. They are (mostly) colorblind-friendly and simply show the facts of climate change: the trend toward a warmer climate is both clear and striking.</p>
       <p>It also allows people to see the effect in their specific place.</p>
       <p>My Region</p>
       <img src="assets/images/ClimateStripesStory1/ArcticStripesWaterisStripes.png">
@@ -214,14 +219,8 @@ onUnmounted(() => {
       <img src="assets/images/ClimateStripesStory1/AKStripesAKShaped.png">
       <p>My City</p>
       <img src="assets/images/ClimateStripesStory1/Fairbanks Climate Stripes 2022.png">
-      <p></p>
-      <Gimme label="Get climate stripes for a community or by lat/long:" class="mt-5" />
-      <div id="chart" class="mb-5"></div>
       <p>But there are shortcomings. If you want to know the precise values of any given year from these stripes you’ll have to dive into the data behind it, and a less clear trend in one specific area can be improperly generalized without the right context.</p>
-      <p>Additionally these stripes typically only display historical data, so they don’t offer insights into   the possible futures in a given place.</p>
-      <p>Unless….</p>
-      <p>With just a few tweaks, they can do precisely that. Read this DataStory to use what you’ve learned about the climate stripes to understand RCPs, SSPs, and our possible climate futures.</p>
-      <p>Also, do you wish you had your own climate stripes for your part of Alaska? Maybe your home town, your hunting spot, your ethnolinguistic region, or your lifelong favorite hydrological unit code level 10? (ours is 1908040302) This JupyterNotebook will help you do just that!</p>
+      <p>Additionally these stripes typically only display historical data, so they don’t offer insights into   the possible futures in a given place. With just a few tweaks, they can do precisely that. Read this DataStory to expand on this story, and understand RCPs, SSPs, and the possible futures climate scientists are trying to understand.</p>
 
     </div>
     <Bios :people="['Mike DeLue']" />
