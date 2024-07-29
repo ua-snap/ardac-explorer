f484dbf184bf97e2f6754dfb9cde933c5797318c
Bruce Crevensten <bruce.crevensten@gmail.com>
2024-07-29 15:27:18 -0700

    interim commit

--- components/FrontPage/Section/Climate.vue
+++ components/FrontPage/Section/Climate.vue
@@ -2,21 +2,19 @@
 
 <template>
   <FrontPageSectionWrapper tag="Climate">
-    <div class="grid is-col-min-12">
-      <div class="cell">
-        <ItemTextPicture slug="climate-beetle-protection" />
-      </div>
-      <div class="cell">
-        <ItemTextPicture slug="indicator-ftc-cmip6" />
-      </div>
-      <div class="cell">
-        <ItemText slug="notebook-design-discharge" />
-      </div>
-      <div class="cell">
-        <ItemText slug="indicator-wsdi" />
-      </div>
-      <div class="cell">
-        <ItemText slug="notebook-raster-database" />
+    <div class="fixed-grid has-3-cols">
+      <div class="grid">
+        <div class="cell border-right">
+          <ItemTextPicture slug="climate-beetle-protection" />
+        </div>
+        <div class="cell border-right">
+          <ItemTextPicture slug="indicator-ftc-cmip6" />
+        </div>
+        <div class="cell">
+          <ItemText slug="notebook-design-discharge" />
+          <ItemText slug="indicator-wsdi" />
+          <ItemText slug="notebook-raster-database" />
+        </div>
       </div>
     </div>
   </FrontPageSectionWrapper>
@@ -24,4 +22,8 @@
 
 <style scoped lang="scss">
 @import '~/assets/styles/items.scss';
+.border-right {
+  padding-right: 1em;
+  border-right: 1px dashed $grey-lighter !important;
+}
 </style>
