315d58dbbf84c1a2108b46948346c985c60b5cef
Craig Stephenson <crstephenson@alaska.edu>
2024-10-25 12:46:56 -0800

    Update CMIP6 Summer Days map on Arctic Climate Data Node story.

--- components/global/StoryArcticClimateDataNode.vue
+++ components/global/StoryArcticClimateDataNode.vue
@@ -10,30 +10,33 @@ const latLng = computed<LatLngValue>(() => placesStore.latLng)
 const layers: MapLayer[] = [
   {
     id: 'indicator_su_historical_era',
-    title: '1980–2009, GFDL-ESM4',
+    title: '1980–2009, TaiESM1',
     source: 'rasdaman',
     wmsLayerName: 'cmip6_indicators',
     style: 'ardac_indicator_su_historical_era',
     legend: 'summer_days',
-    rasdamanConfiguration: { dim_model: 4, dim_scenario: 0 },
+    rasdamanConfiguration: { dim_model: 11, dim_scenario: 0 },
+    coastline: true,
   },
   {
     id: 'indicator_su_midcentury_era',
-    title: '2040–2069, GFDL-ESM4, SSP5-8.5',
+    title: '2040–2069, TaiESM1, SSP5-8.5',
     source: 'rasdaman',
     wmsLayerName: 'cmip6_indicators',
     style: 'ardac_indicator_su_midcentury_era',
     legend: 'summer_days',
-    rasdamanConfiguration: { dim_model: 4, dim_scenario: 4 },
+    rasdamanConfiguration: { dim_model: 11, dim_scenario: 4 },
+    coastline: true,
   },
   {
     id: 'indicator_su_latecentury_era',
-    title: '2070–2099, GFDL-ESM4, SSP5-8.5',
+    title: '2070–2099, TaiESM1, SSP5-8.5',
     source: 'rasdaman',
     wmsLayerName: 'cmip6_indicators',
     style: 'ardac_indicator_su_latecentury_era',
     legend: 'summer_days',
-    rasdamanConfiguration: { dim_model: 4, dim_scenario: 4 },
+    rasdamanConfiguration: { dim_model: 11, dim_scenario: 4 },
+    coastline: true,
   },
 ]
 
@@ -222,9 +225,9 @@ onMounted(() => {
           Summer days are the number of days per year that are above 25&deg;C
           &lpar;77&deg;F&rpar;. The map below shows the 30-year mean of summer
           days using CMIP6 temperature data for three eras. The historical era
-          (1980&ndash;2009) uses model baseline data from the GFDL-ESM4 dataset.
+          (1980&ndash;2009) uses model baseline data from the TaiESM1 dataset.
           The mid-century era (2040&ndash;2069) and late-century era
-          (2070&ndash;2099) use GFDL-ESM4 data under the climate scenario
+          (2070&ndash;2099) use TaiESM1 data under the climate scenario
           SSP5-8.5 scenario.
         </p>
       </div>
