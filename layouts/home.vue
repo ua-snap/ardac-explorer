927b14b66fd13ac43c9175d39915c593f02b450d
Bruce Crevensten <bruce.crevensten@gmail.com>
2024-10-25 11:29:53 -0700

    Adds trumpet

--- layouts/home.vue
+++ layouts/home.vue
@@ -8,12 +8,18 @@
   <div class="container is-fullhd">
     <Header />
     <section class="section">
-      <div class="content is-size-3">
-        <p>
+      <div class="content">
+        <p class="is-size-3">
           The Arctic Data Collaborative (ARDAC) provides highly-curated,
           relevant <b>Arctic datasets</b>, streamlined <b>data access</b>,
           processing <code>code examples</code> and <b>visualizations</b> to
           support research in the North.
+        </p>
+        <p class="trumpet is-size-4">
+          <span>📢</span>
+          <NuxtLink to="/tag/CMIP6"
+            >check out just-added CMIP6 datasets!</NuxtLink
+          >
         </p>
       </div>
     </section>
@@ -23,4 +29,13 @@
   <Footer />
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.trumpet span {
+  display: inline-block;
+  margin-right: 0.75rem;
+  -webkit-transform: rotateY(180deg);
+  -moz-transform: rotateY(180deg);
+  -o-transform: rotateY(180deg);
+  -ms-transform: rotateY(180deg);
+}
+</style>
