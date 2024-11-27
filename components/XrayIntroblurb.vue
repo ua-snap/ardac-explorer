cfd78675aac35a292e005d34a4c27e1c4e272e9d
MikeDelue <mdelue@alaska.edu>
2024-11-27 14:31:01 -0900

    Tweaks to xray_introbox text

--- components/XrayIntroblurb.vue
+++ components/XrayIntroblurb.vue
@@ -4,12 +4,12 @@ const props = defineProps(['resolution', 'unit'])
 
 <template>
   <blockquote class="xray--introblurb">
-    <p><span class="robot">🔬</span> This page is a <strong>data x-ray</strong> which lets you view <em>maps</em> and get <em>charts</em> from this dataset.</p>
-    <p>This dataset shows projected climate model output.  Here&rsquo;s some key acronynms and information to know about:</p>
+    <p><span class="robot">🔬</span> This is a <strong>dataset x-ray</strong> which lets you view <em>maps</em> and get <em>charts</em> from this dataset.</p>
+    <p>This dataset shows projected climate model output.  Here&rsquo;s some key relevant information:</p>
     <ul>
-      <li>GDFL-ESM4, CESM2, etc… these are the names of climate models. <a href="">&#x2192; read more</a></li>
-      <li>SSP1-2.5, SSP2-4.5 etc… these are socioeconomic and emission scenarios. <a href="">&#x2192; read more</a></li>
-      <li>this dataset is at a {{ resolution }}&#8239;{{ unit }} spatial resolution</li>
+      <li>GDFL-ESM4, CESM2, etc… these are the names of climate models. <a href="https://doi.org/10.1016/j.envsoft.2018.03.021">&#x2192; read more</a></li>
+      <li>SSP1-2.5, SSP2-4.5 etc… these are socioeconomic and emission scenarios. <a href="https://glisa.umich.edu/wp-content/uploads/2021/03/A_Practitioners_Guide_to_Climate_Model_Scenarios.pdf">&#x2192; read more</a></li>
+      <li>the spatial resolution of this dataset is {{ resolution }}&#8239;{{ unit }}</li>
     </ul>
   </blockquote>
 </template>
