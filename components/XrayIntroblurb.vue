d9552271294fc9a52d914fdaa4ba48cfd7f4b7f9
Bruce Crevensten <bruce.crevensten@gmail.com>
2024-12-13 12:40:26 -0800

    Fix typo which made Not Peer Reviewed show up everywhere

--- components/XrayIntroblurb.vue
+++ components/XrayIntroblurb.vue
@@ -7,7 +7,7 @@ const props = defineProps(['resolution', 'unit', 'cmip', 'beta'])
     <p><span class="robot">🔬</span> This is a <strong>dataset x-ray</strong> which lets you view <em>maps</em>, get <em>charts</em>, and <em>drill</em> into this dataset to get a CSV of data for a place.</p>
     <p v-if="cmip">This dataset shows projected climate model output.  Here&rsquo;s some key relevant information:</p>
     <ul>
-      <li vi-if="beta">⚠️ This dataset, while accurate and of good quality, <strong>does not have a peer-reviewed publication associated with it yet</strong>. &#x2192; <a href="mailto:uaf-snap-data-tools@alaska.edu">get in touch</a> about similar data which are peer reviewed</li>
+      <li v-if="beta">⚠️ This dataset, while accurate and of good quality, <strong>does not have a peer-reviewed publication associated with it yet</strong>. &#x2192; <a href="mailto:uaf-snap-data-tools@alaska.edu">get in touch</a> about similar data which are peer reviewed</li>
       <li v-if="cmip == 6">GDFL-ESM4, CESM2, etc&hellip; these are the names of climate models. <a href="https://doi.org/10.1016/j.envsoft.2018.03.021">&#x2192; read more</a></li>
       <li v-if="cmip == 5">NCAR CCSM4, MRI CGCM3, etc&hellip; these are the names of climate models.</li>
       <li v-if="cmip == 6">SSP1-2.5, SSP2-4.5 etc… these are socioeconomic and emission scenarios. <a href="https://glisa.umich.edu/wp-content/uploads/2021/03/A_Practitioners_Guide_to_Climate_Model_Scenarios.pdf">&#x2192; read more</a></li>
