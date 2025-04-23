6083b9cca69ebcde9c9348777950ad3224854481
Bruce Crevensten <bruce.crevensten@gmail.com>
2025-04-23 10:46:28 -0700

    Moves the CSV-by-region back into Places item

--- components/GetAndUseData.vue
+++ components/GetAndUseData.vue
@@ -21,79 +21,7 @@ const props = defineProps({
     type: String,
     required: false,
   },
-  pointDataRegion: {
-    type: String,
-    required: false,
-    validator: (value: string) =>
-      [
-        'alaska',
-        'alberta',
-        'british_columbia',
-        'faroe',
-        'finland',
-        'greenland',
-        'iceland',
-        'manitoba',
-        'newfoundland_and_labrador',
-        'northwest_territories',
-        'norway',
-        'nunavut',
-        'ontario',
-        'quebec',
-        'russia',
-        'saskatchewan',
-        'sweden',
-        'yukon',
-        'all',
-      ].includes(value),
-  },
 })
-
-const formattedRegion = computed(() => {
-  if (props.pointDataRegion === 'all') return ''
-  return props.pointDataRegion
-    ?.replace(/_/g, ' ')
-    .split(' ')
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(' ')
-})
-
-const regionsList = computed(() => [
-  { name: 'Alaska', key: 'alaska', countryCode: 'US' },
-  { name: 'Alberta', key: 'alberta', countryCode: 'CA' },
-  { name: 'British Columbia', key: 'british_columbia', countryCode: 'CA' },
-  { name: 'Faroe Islands', key: 'faroe', countryCode: 'FO' },
-  { name: 'Finland', key: 'finland', countryCode: 'FI' },
-  { name: 'Greenland', key: 'greenland', countryCode: 'GL' },
-  { name: 'Iceland', key: 'iceland', countryCode: 'IS' },
-  { name: 'Manitoba', key: 'manitoba', countryCode: 'CA' },
-  {
-    name: 'Newfoundland and Labrador',
-    key: 'newfoundland_and_labrador',
-    countryCode: 'CA',
-  },
-  {
-    name: 'Northwest Territories',
-    key: 'northwest_territories',
-    countryCode: 'CA',
-  },
-  { name: 'Norway', key: 'norway', countryCode: 'NO' },
-  { name: 'Nunavut', key: 'nunavut', countryCode: 'CA' },
-  { name: 'Ontario', key: 'ontario', countryCode: 'CA' },
-  { name: 'Quebec', key: 'quebec', countryCode: 'CA' },
-  { name: 'Russia', key: 'russia', countryCode: 'RU' },
-  { name: 'Saskatchewan', key: 'saskatchewan', countryCode: 'CA' },
-  { name: 'Sweden', key: 'sweden', countryCode: 'SE' },
-  { name: 'Yukon', key: 'yukon', countryCode: 'CA' },
-])
-
-const getFlagEmoji = (countryCode: string) => {
-  const codePoints = countryCode
-    .toUpperCase()
-    .split('')
-    .map(char => 127397 + char.charCodeAt(0))
-  return String.fromCodePoint(...codePoints)
-}
 </script>
 
 <template>
@@ -102,42 +30,6 @@ const getFlagEmoji = (countryCode: string) => {
     <slot name="preamble"></slot>
     <div class="content is-size-5">
       <ul>
-        <li v-if="props.pointDataRegion === 'all'">
-          Download point location CSV files for:
-          <div class="columns is-multiline mt-2 mb-3">
-            <div
-              class="column is-one-third"
-              v-for="(col, index) in 3"
-              :key="index"
-            >
-              <ul class="region-list">
-                <li
-                  v-for="region in regionsList.slice(
-                    index * 6,
-                    (index + 1) * 6
-                  )"
-                  :key="region.key"
-                >
-                  <span class="mr-2">{{
-                    getFlagEmoji(region.countryCode)
-                  }}</span>
-                  <a
-                    :href="`https://github.com/ua-snap/geospatial-vector-veracity/raw/main/vector_data/point/${region.key}_point_locations.csv`"
-                  >
-                    {{ region.name }}
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </li>
-        <li v-else-if="props.pointDataRegion">
-          <a
-            :href="`https://github.com/ua-snap/geospatial-vector-veracity/raw/main/vector_data/point/${props.pointDataRegion}_point_locations.csv`"
-          >
-            Download {{ formattedRegion }} CSV file
-          </a>
-        </li>
         <li v-if="props.presentInNcr && props.presentInEds">
           Access this data in the context of climate change projections at
           <a href="https://northernclimatereports.org"
@@ -184,18 +76,4 @@ const getFlagEmoji = (countryCode: string) => {
   </div>
 </template>
 
-<style scoped>
-.region-list {
-  list-style: none;
-  margin-left: 0;
-
-  li {
-    margin-bottom: 0.25rem;
-    line-height: 1.25;
-  }
-
-  a {
-    font-size: 0.9em;
-  }
-}
-</style>
+<style scoped></style>
