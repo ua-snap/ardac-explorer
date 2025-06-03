0782d7880a37e31d218d4699a816f6c7ab8147a1
Craig Stephenson <crstephenson@alaska.edu>
2025-06-03 13:32:51 -0800

    Update edition month to June in home page header.

--- components/Edition.vue
+++ components/Edition.vue
@@ -2,14 +2,15 @@
 
 <template>
   <div class="edition">
-    &mdash; <span class="warn">PRE-RELEASE</span>&nbsp;&nbsp;🌸&nbsp;Edition, May 2025 &mdash;
+    &mdash; <span class="warn">PRE-RELEASE</span>&nbsp;&nbsp;🌸&nbsp;Edition,
+    June 2025 &mdash;
   </div>
 </template>
 
 <style scoped lang="scss">
 .edition {
   text-align: center;
-  font-family: "Patrick Hand";
+  font-family: 'Patrick Hand';
   font-size: 2rem;
   color: $gray;
   .warn {
